7465171e289a000c8a4ecbae26e6c00e13244ccc
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2019-01-11 09:33:01 +0100

    Firebase connection is done

--- app/src/main/java/com/example/j_lds/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/j_lds/firebasetest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.j_lds.firebasetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Firebase connection done", Toast.LENGTH_SHORT).show();
     }
 }
