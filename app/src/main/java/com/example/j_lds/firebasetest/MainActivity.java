4e433b9751bf8bed7f228ceea1715008fae0c62e
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2019-01-11 10:34:29 +0100

    can insert data in firebase database

--- app/src/main/java/com/example/j_lds/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/j_lds/firebasetest/MainActivity.java
@@ -2,9 +2,22 @@ package com.example.j_lds.firebasetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et_fn, et_ln, et_pwd, et_cpwd, et_birth;
+    private String fn, ln, pwd, cpwd, birth;
+    private Button btn_save;
+    private User user;
+
+    private DatabaseReference mdatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +25,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "Firebase connection done", Toast.LENGTH_SHORT).show();
+
+        et_fn = (EditText)findViewById(R.id.editText_firstName);
+        et_ln = (EditText)findViewById(R.id.editText_lastName);
+        et_pwd = (EditText)findViewById(R.id.editText_password);
+        et_cpwd = (EditText)findViewById(R.id.editText_confirm_password);
+        et_birth = (EditText)findViewById(R.id.editText_birthday);
+
+        btn_save = (Button) findViewById(R.id.button_save);
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                test();
+            }
+        });
+
+        mdatabaseReference = FirebaseDatabase.getInstance("https://test-jl010.firebaseio.com/").getReference().child("user");
+    }
+
+    private void test(){
+        fn = et_fn.getText().toString().trim();
+        ln = et_ln.getText().toString().trim();
+        pwd = et_pwd.getText().toString().trim();
+        cpwd = et_cpwd.getText().toString().trim();
+        birth = et_birth.getText().toString().trim();
+
+        if (pwd.equals(cpwd) && !pwd.equals("") && !cpwd.equals("")){
+            user = new User();
+            user.setFirstName(fn);
+            user.setLastName(ln);
+            user.setPassword(pwd);
+            user.setBirth(birth);
+
+            mdatabaseReference.setValue(user);
+            Toast.makeText(this, "Information is saved !!!", Toast.LENGTH_SHORT).show();
+
+        }else{
+            Toast.makeText(this, "The password id not the same", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/j_lds/firebasetest/User.java
@@ -0,0 +1,43 @@
+package com.example.j_lds.firebasetest;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String birth;
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+}
